6899f47ffd963c8b4353637ccd1c93ad7ea9ea93
Juan Galarce <j6alarce@gmail.com>
2021-03-05 12:28:02 -0300

    Player
    Se crea el script para el movimiento y salto del player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D theRB;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+    }
+}
